e2612fe450d317c435d925f0e3f67ed730d60161
Dipayan12342 <duttadipayan406@gmail.com>
2021-01-11 14:43:55 +0530

    added log

--- sketch.js
+++ sketch.js
@@ -23,6 +23,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    log22 = new Log(120,20,50,PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -58,6 +59,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log22.display();
 
     bird.display();
     platform.display();
